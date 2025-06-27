80e9e8d638ac1a605919846cc85ac74fdb5e836c
AlexeyOblako <chishkoalex@gmail.com>
2025-06-27 14:15:38 +0300

    #7 learn Archakov 10:09

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './scss/app.scss';
+import  Header from './components/Header';
+import Sort from "./components/Sort";
+import Categories from './components/Categories';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+
+import pizzas from './assets/pizzas.json';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://685a69ed9f6ef96111564553.mockapi.io/Items')
+        .then(res => res.json())
+        .then(arr => {setItems(arr)});
+    }, []);
+
+
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {pizzas.map(obj => <PizzaBlock key={obj.id} {...obj}/>)}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+
+function Categories() {
+
+    const [activeIndex, setActiveIndex] = React.useState(0);
+
+    const onClickLi = (index) => {
+        setActiveIndex(index);
+
+    }
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
+
+    return (
+        <div className="categories">
+            <ul>
+                {categories.map((item, index) => <li key={item} onClick={() => onClickLi(index)} className={activeIndex === index ? 'active' : ''}>{item}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default Categories;
